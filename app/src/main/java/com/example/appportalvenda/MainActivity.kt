a23f57349d6e7155fb0ed21b7e0473376ea58498
Arthur Pereira Machado <machadoparthur1@gmail.com>
2020-03-02 22:54:12 -0300

    Starting to store the acces

--- app/src/main/java/com/example/appportalvenda/MainActivity.kt
+++ app/src/main/java/com/example/appportalvenda/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.appportalvenda
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -7,6 +8,7 @@ import android.webkit.WebView
 import kotlinx.android.synthetic.*
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_net_shoes.*
+import kotlinx.android.synthetic.main.activity_sobre.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,21 +17,35 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         picNetShoes.setOnClickListener({
+            // Gravar as vezes que foi clicado
+            var sh = getSharedPreferences("acessos", Context.MODE_PRIVATE);
+            sh.edit().putInt("qtde", resAcesso1.text.toString().toInt()).apply();
+
+            // Redirecionar para outra tela
             var NetShoesIntent = Intent(this, NetShoes::class.java);
             startActivity(NetShoesIntent);
         });
 
         picMercadoLivre.setOnClickListener({
+
+
+            // Redirecionar para outra tela
             var MercadoLivreIntent = Intent(this, MercadoLivre::class.java);
             startActivity(MercadoLivreIntent);
         });
 
         picAmericanas.setOnClickListener({
+
+
+            // Redirecionar para outra tela
             var AmericanasIntent = Intent(this, Americanas::class.java);
             startActivity(AmericanasIntent);
         })
 
         btnSobre.setOnClickListener({
+
+
+            // Redirecionar para outra tela
             var SobreIntent = Intent(this, Sobre::class.java);
             startActivity(SobreIntent);
         })
--- app/src/main/java/com/example/appportalvenda/Sobre.kt
+++ app/src/main/java/com/example/appportalvenda/Sobre.kt
@@ -1,12 +1,15 @@
 package com.example.appportalvenda
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_sobre.*
 
 class Sobre : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sobre)
+
     }
 }
